63a665b63f5df9013d960fc944b6c0a895e5e6ca
Efrain Rojas <erojas@zenlabs.net>
2018-05-29 10:56:59 -0400

    others changes

--- app/controllers/spree/pagos_net_controller.rb
+++ app/controllers/spree/pagos_net_controller.rb
@@ -2,7 +2,6 @@ module Spree
   class PagosNetController < ApplicationController
 
     def update
-      debugger
     #   @payment_method = PaymentMethod.find params[:payment_method_id]
     #   data = JSON.parse Base64.strict_decode64 params[:data]
     #   render text: "Bad signature\n", status: 401 and return unless @payment_method.check_signature params[:data], params[:signature]
--- app/models/spree/payment_method/pagos_net.rb
+++ app/models/spree/payment_method/pagos_net.rb
@@ -1,43 +1,45 @@
 module Spree
-  class PaymentMethod::PagosNet < PaymentMethod
-    preference :server, :string, default: 'http://localhost:3000'
-    preference :account_pagos_net, :string
-    preference :password_pagos_net, :string
-    preference :public_key, :string, default: ''
-    preference :private_key, :string, default: ''
-    preference :order_description, :string, default: -> { Spree::Store.current.name }
-    preference :test_mode, :boolean, default: true
+  module PaymentMethod
+    class PagosNet < PaymentMethod
+      preference :server, :string, default: 'http://localhost:3000'
+      preference :account_pagos_net, :string
+      preference :password_pagos_net, :string
+      preference :public_key, :string, default: ''
+      preference :private_key, :string, default: ''
+      preference :order_description, :string, default: -> { Spree::Store.current.name }
+      preference :test_mode, :boolean, default: true
 
-    def provider_class
-      ActiveMerchant::Billing::PagosNet
-    end
+      def provider_class
+        ActiveMerchant::Billing::PagosNet
+      end
 
-    def provider
-      @provider ||= provider_class.new(preferred_public_key, preferred_private_key)
-    end
+      def provider
+        @provider ||= provider_class.new(preferred_public_key, preferred_private_key)
+      end
 
-    def source_required?
-      false
-    end
+      def source_required?
+        false
+      end
 
-    def checkout_url
-      "#{preferred_server}/spree/pagos_net"
-    end
+      def checkout_url
+        "#{preferred_server}/spree/pagos_net"
+      end
 
-    def cnb_form_fields(order, result_url, server_url)
-      provider.cnb_form_fields amount: order.total,
-                               currency: order.currency,
-                               description: preferred_order_description,
-                               order_id: order.id,
-                               result_url: result_url,
-                               server_url: server_url,
-                               account_pagos_net: '',
-                               password_pagos_net: '',
-                               sandbox: preferred_test_mode ? 1 : 0
-    end
+      def cnb_form_fields(order, result_url, server_url)
+        provider.cnb_form_fields amount: order.total,
+                                 currency: order.currency,
+                                 description: preferred_order_description,
+                                 order_id: order.id,
+                                 result_url: result_url,
+                                 server_url: server_url,
+                                 account_pagos_net: '',
+                                 password_pagos_net: '',
+                                 sandbox: preferred_test_mode ? 1 : 0
+      end
 
-    def check_signature(data, signature)
-      provider.check_signature data, signature
+      def check_signature(data, signature)
+        provider.check_signature data, signature
+      end
     end
   end
 end
