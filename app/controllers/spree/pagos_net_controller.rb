c443f561a4b4ab9a3b601b7419518040b69f3249
Efrain Rojas <erojas@zenlabs.net>
2018-05-29 11:17:51 -0400

    change payment method

--- app/controllers/spree/pagos_net_controller.rb
+++ app/controllers/spree/pagos_net_controller.rb
@@ -2,6 +2,7 @@ module Spree
   class PagosNetController < ApplicationController
 
     def update
+      debugger
     #   @payment_method = PaymentMethod.find params[:payment_method_id]
     #   data = JSON.parse Base64.strict_decode64 params[:data]
     #   render text: "Bad signature\n", status: 401 and return unless @payment_method.check_signature params[:data], params[:signature]
