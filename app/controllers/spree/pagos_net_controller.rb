b93cf499f063203426f1db046ee5e227f0723e5c
Efrain Rojas <erojas@zenlabs.net>
2018-05-29 11:26:51 -0400

    update to payment method

--- app/controllers/spree/pagos_net_controller.rb
+++ app/controllers/spree/pagos_net_controller.rb
@@ -1,23 +1,35 @@
 module Spree
   class PagosNetController < ApplicationController
+    before_action :set_order, only: [:update]
+    before_action :set_payment_method, only: [:update]
 
     def update
       debugger
-    #   @payment_method = PaymentMethod.find params[:payment_method_id]
-    #   data = JSON.parse Base64.strict_decode64 params[:data]
-    #   render text: "Bad signature\n", status: 401 and return unless @payment_method.check_signature params[:data], params[:signature]
-    #   @order = Order.find data['order_id']
-    #
-    #   raise ArgumentError unless @order.payments.completed.empty? &&
-    #     data['currency'] == @order.currency &&
-    #     BigDecimal(data['amount']) == @order.total &&
-    #     data['type'] == 'buy' &&
-    #     (data['status'] == 'success' || (@payment_method.preferred_test_mode && data['status'] == 'sandbox'))
-    #
-    #   payment = @order.payments.create amount: @order.total, payment_methods: @payment_method
-    #   payment.complete!
-    #
-    #   render text: "Thank you.\n"
+      #   @payment_method = PaymentMethod.find params[:payment_method_id]
+      #   data = JSON.parse Base64.strict_decode64 params[:data]
+      #   render text: "Bad signature\n", status: 401 and return unless @payment_method.check_signature params[:data], params[:signature]
+      #   @order = Order.find data['order_id']
+      #
+      #   raise ArgumentError unless @order.payments.completed.empty? &&
+      #     data['currency'] == @order.currency &&
+      #     BigDecimal(data['amount']) == @order.total &&
+      #     data['type'] == 'buy' &&
+      #     (data['status'] == 'success' || (@payment_method.preferred_test_mode && data['status'] == 'sandbox'))
+      #
+      #   payment = @order.payments.create amount: @order.total, payment_methods: @payment_method
+      #   payment.complete!
+      #
+      #   render text: "Thank you.\n"
     end
+  end
+
+  private
+
+  def set_order
+    @order = Spree::Order.find(params['order_id'])
+  end
+
+  def set_payment_method
+    @payment_method = PaymentMethod.find(params['order']['payments_attributes'][0]['payment_method_id'])
   end
 end
