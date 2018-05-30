ebe27433bd4e8eec3cc2683539d2d74f62cb42fe
Efrain Rojas <erojas@zenlabs.net>
2018-05-30 12:51:00 -0400

    update to payment complete

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -1,17 +1,59 @@
 module Spree
   CheckoutController.class_eval do
-    def pagos_net_result
-      update if has_completed_payments?
-    end
+    before_action :set_type_pagos_net, only: [:update]
+    # before_action :set_order_net, only: [:update]
+    before_action :set_payment_method_net, only: [:update]
 
-    def pagos_net_status
-      render json: {completed: has_completed_payments?}
+    def update
+      if @type_pagos_net
+        pagos_net = PagosNet.new(@payment_method_p.id)
+        user = { 'id' => @order.user.id,
+                 'fiscal_name' => params['name_invoice'],
+                 'ci' => params['ci_invoice'],
+                 'email' => "user_ts_#{@order.user.id}@gmail.com" }
+        rspn = pagos_net.create_transaction(@order.number,
+                                            @order.total,
+                                            user,
+                                            @type_pagos_net)
+        rspn_pagosnet = { 'status' => rspn.body[:registro_plan_response][:return][:codigo_error],
+                          'message' => rspn.body[:registro_plan_response][:return][:descripcion_error],
+                          'id_transaccion' => rspn.body[:registro_plan_response][:return][:id_transaccion] }
+        logger.info(rspn_pagosnet)
+      end
+      if @order.update_from_params(params, permitted_checkout_attributes, request.headers.env)
+        @order.temporary_address = !params[:save_user_address]
+        unless @order.next
+          flash[:error] = @order.errors.full_messages.join("\n")
+          redirect_to(checkout_state_path(@order.state)) && return
+        end
+
+        if @order.completed?
+          @current_order = nil
+          flash.notice = Spree.t(:order_processed_successfully)
+          flash['order_completed'] = true
+          redirect_to completion_route
+        else
+          redirect_to checkout_state_path(@order.state)
+        end
+      else
+        render :edit
+      end
     end
 
     private
 
-    def has_completed_payments?
-      @order.payments.completed.any?
+    def set_type_pagos_net
+      @type_pagos_net = if params.key?('type_pagos_net')
+                          if params['type_pagos_net'] == 'credit_cart'
+                            '2'
+                          else
+                            '1'
+                          end
+                        end
+    end
+    
+    def set_payment_method_net
+      @payment_method_p = PaymentMethod.find(params['order']['payments_attributes'][0]['payment_method_id']) rescue nil
     end
   end
 end
