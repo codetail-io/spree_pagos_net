62fdabbb34a081936735a15ca48b363e4d10cc3d
Julio Martinez <jmartinez@zenlabs.net>
2018-05-30 15:33:00 -0400

    remove routes action.wsdl

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,5 @@ Spree::Core::Engine.add_routes do
   end
   patch '/spree/pagos_net', to: "pagos_net#update", as: :pagos_net_update
 end
-get '/tushop_webservice/action.WSDL', to: redirect('/tushop_webservice/action')
+# get '/tushop_webservice/action.WSDL', to: redirect('/tushop_webservice/action')
 wash_out :tushop_webservice
