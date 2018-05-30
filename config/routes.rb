41bcf99a7d9d227853839672022c07550d4cc8ab
Julio Martinez <jmartinez@zenlabs.net>
2018-05-30 15:28:06 -0400

    include wash-out.

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Spree::Core::Engine.add_routes do
       end
     end
   end
-
   patch '/spree/pagos_net', to: "pagos_net#update", as: :pagos_net_update
 end
+get '/tushop_webservice/action.WSDL', to: redirect('/tushop_webservice/action')
+wash_out :tushop_webservice
