4e0813ba64fcbabf885943739caeb0c3d7cc1338
Efrain Rojas <erojas@zenlabs.net>
2018-06-01 17:10:48 -0400

    rubocop ofenses

--- app/controllers/spree/pagos_net_controller.rb
+++ app/controllers/spree/pagos_net_controller.rb
@@ -3,14 +3,13 @@ module Spree
     before_action :set_order, only: [:credit_card]
 
     def credit_card
-      render 'spree/pagos_net/index'
+      render 'spree/pagos_net/index' if @order
     end
 
     private
 
     def set_order
-      debugger
-      @order = Spree::Order.find(params['id'].to_i)
+      @order = Spree::Order.find(params['id'].to_i) rescue nil
     end
   end
 end
