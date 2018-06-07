8b55f0dd5eddcf60000b79a01732701033704753
Efrain Rojas <erojas@zenlabs.net>
2018-06-07 10:21:55 -0400

    update to complete status

--- app/models/spree/payment_method/pagos_net.rb
+++ app/models/spree/payment_method/pagos_net.rb
@@ -10,8 +10,6 @@ module Spree
     preference :credit_card_nro_client, :string, default: 68
     preference :credit_card_entity, :string, default: 115
     preference :credit_card_key_authenticate, :string, default: 'JEn1jJsD5hFrip4jzHODyA=='
-    preference :public_key, :string, default: ''
-    preference :private_key, :string, default: ''
     preference :test_mode, :boolean, default: true
 
     def provider_class
@@ -19,7 +17,7 @@ module Spree
     end
 
     def provider
-      @provider ||= provider_class.new(preferred_public_key, preferred_private_key)
+      @provider ||= provider_class.new
     end
 
     def source_required?
@@ -30,20 +28,5 @@ module Spree
       "#{preferred_server}/spree/pagos_net"
     end
 
-    def cnb_form_fields(order, result_url, server_url)
-      provider.cnb_form_fields amount: order.total,
-                               currency: order.currency,
-                               description: preferred_order_description,
-                               order_id: order.id,
-                               result_url: result_url,
-                               server_url: server_url,
-                               account_pagos_net: '',
-                               password_pagos_net: '',
-                               sandbox: preferred_test_mode ? 1 : 0
-    end
-
-    def check_signature(data, signature)
-      provider.check_signature data, signature
-    end
   end
 end
